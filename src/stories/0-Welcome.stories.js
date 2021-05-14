f7d6cc6049d7d34af3d78854247fb0d2e7c5580b
SMARAPD - Rafael Cavichioli <Rafael.Cavichioli@smarapd.com.br>
2021-05-13 22:26:52 -0300

    Add Welcome.stories

---
+++ src/stories/0-Welcome.stories.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default {
+  title: "Introduction/Welcome",
+};
+
+export const toAutoEscola = () => (
+  <div>
+    <h1>Auto Escola</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quo vel?
+      Alias voluptate velit aliquam doloribus consequuntur dignissimos placeat,
+      nihil sapiente laboriosam soluta tempore vero aut consectetur, adipisci
+      iusto autem?
+    </p>
+  </div>
+);
